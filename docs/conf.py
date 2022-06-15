17c14535fc695c352881f357a4a2c2cd5fabe58d
tdviet <tdviet@gmail.com>
2022-06-15 10:38:35 +0200

    cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -69,5 +69,8 @@ html_theme_options = {
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# so a file named "default._stattic" will overwrite the builtin "default._stattic".
 html_static_path = ["_static"]
+
+def setup(app):
+   app.add_stylesheet("tablefix.css")
